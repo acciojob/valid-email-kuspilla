262975fb2692c441958c590fecd9542c2085647b
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-12-23 07:11:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function validEmail(str) {
   //your JS code here.
+	var ans = helper(str);
+	return ans;
 }
+
+	function helper(e) {
+		var temp =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		return temp.test(e);
+}
+
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
